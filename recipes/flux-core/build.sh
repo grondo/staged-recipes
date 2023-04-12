ec6b6ad1922798d73d71ad3844c3d502969bff42
Mark A. Grondona <mark.grondona@gmail.com>
2023-04-12 15:29:33 +0000

    testing

--- recipes/flux-core/build.sh
+++ recipes/flux-core/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export CPPFLAGS="-D_FORTIFY_SOURCE=2 -O2 -isystem $PREFIX/include"
 ./configure --prefix=${PREFIX}
-make
-make check 
+make -j 12
+make -j 6 check-prep
+make -j 6 check
 make install
